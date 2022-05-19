d680fce10ae5b968ebccc52b08903a8e418f9df6
Abylai <aba.omar@mail.ru>
2022-05-19 15:38:50 +0600

    Peremeshka Massiva

---
+++ Task_Peremeshka_Massiva/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] numbers = {1, 2, 3, 4, 5, 6};
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int randomIndex = new Random().Next(0, numbers.Length);
+    int temp = numbers[i];
+    numbers[i] = numbers[randomIndex];
+    numbers[randomIndex] = temp;
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
