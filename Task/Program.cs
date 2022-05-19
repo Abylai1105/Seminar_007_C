16f2dac371571d394cf8e410d7d6bd85abed7005
Abylai <aba.omar@mail.ru>
2022-05-19 15:24:52 +0600

    Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string output = "3, 56, 78, 4, 5";
+
+int countNumbers = 1;
+for (int i = 0; i < output.Length; i++)
+{
+    if (output[i] == ',')
+    countNumbers++;
+}
+
+int[] numbers = new int[countNumbers];
+
+int numbersIndex = 0;
+for (int i = 0; i < output.Length; i++)
+{
+    string subString = String.Empty;
+
+    while (output[i] != ',')
+    {
+        if (i >= output.Length - 1)
+        break;
+        subString += output[i].ToString();
+        i++;
+    }
+
+    if (subString == "" ||subString == " ")
+        continue;
+
+    numbers[numbersIndex] = Convert.ToInt32(subString);
+    numbersIndex++;
+
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+
+
