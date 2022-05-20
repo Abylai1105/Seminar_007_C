6a4431034f09838443f53f165d364c1e178938ed
Abylai <aba.omar@mail.ru>
2022-05-20 15:37:35 +0600

    Task050

---
+++ Task050/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, 
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+Console.Write("Задайте m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] array = new int [m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array [i, j] = new Random().Next(1, 11);
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int sum = 0;
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i == j)
+        {
+            sum = sum + array[i, j];
+        }  
+    }   
+}
+  Console.WriteLine(sum);
